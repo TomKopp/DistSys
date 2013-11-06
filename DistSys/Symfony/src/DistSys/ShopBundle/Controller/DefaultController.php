1a757600f932666f53d306e1826665e02573e5b2
Tobias Dietrich (Selen) <info@etooby.com>
2013-11-07 00:11:19 +0100

    simpleSite

--- DistSys/Symfony/src/DistSys/ShopBundle/Controller/DefaultController.php
+++ DistSys/Symfony/src/DistSys/ShopBundle/Controller/DefaultController.php
@@ -7,13 +7,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller {
 
   public function indexAction($url) {
+  	
     if (strcmp($url, 'frontpage') !== 0) {
-      $simpleSite = $this->getDoctrine()->getRepository('DistSysShopBundle:SimpleSite')->findByTitle($url);
-      if (is_a($simpleSite, 'SimpleSite')) {
+    	
+      $simpleSite = $this->getDoctrine()->getRepository('DistSysShopBundle:SimpleSite')->findOneByTitle($url);
         return $this->render('DistSysShopBundle:Default:index.html.twig', array('url' => $url, 'site' => $simpleSite));
-      } else {
-        return $this->render('DistSysShopBundle:Default:index.html.twig', array('url' => $url));
-      }
+
     } else {
       return $this->render('DistSysShopBundle:Default:index.html.twig', array('url' => $url));
     }
